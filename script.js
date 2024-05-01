a276467cf0e59f6f80c60e66c89aef373ad343bd
Samiksha2212 <samiksha23@navgurukul.org>
2024-05-01 10:02:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let Div = document.createElement("div");
+document.body.append(Div);
+Div.style.borderWidth="3px";
+Div.style.border="solid"
+Div.style.width="50vw";
+Div.style.height="180px"
+Div.style.borderRadius="10px"
+Div.style.textAlign="center"
+let  heading = document.createElement("h1");
+Div.appendChild(heading);
+heading.textContent="Tourism";
+heading.style.color="green"
+let btn = document.createElement("button");
+Div.appendChild(btn)
+btn.textContent="submit";
+btn.style.width="100px"
+btn.style.height="40px"
+btn.style.borderWidth="0px"
+btn.style.color="green"
+btn.style.backgroundColor="lightgreen";
+btn.style.borderRadius="8px";
+
+
+
